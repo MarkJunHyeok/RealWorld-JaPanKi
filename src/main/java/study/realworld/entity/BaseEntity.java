ac8dfede8f7511c9c4b8e405b81a501f31902a5b
peubel <sksqkrwns@naver.com>
2022-04-12 20:44:08 +0900

    User Entity 추가 및 테스트

--- src/main/java/study/realworld/entity/BaseEntity.java
+++ src/main/java/study/realworld/entity/BaseEntity.java
@@ -4,14 +4,18 @@ package study.realworld.entity;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
-import javax.persistence.*;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 
 @Getter
 @EqualsAndHashCode
 @MappedSuperclass
 public class BaseEntity {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 }
 
--- src/main/resources/db/migration/V1_0_0__add_Human.sql
+++
@@ -1,7 +0,0 @@
-create table Human
-(
-    id bigint  not null auto_increment,
-    money integer,
-    name varchar(255),
-    primary key (id)
-);
